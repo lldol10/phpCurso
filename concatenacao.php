4758204882aa0251a8039453f7d3688a3e399a29
Luan <luan.lucas@live.com>
2021-10-16 21:51:29 -0300

    aulas de concatenação e variaveis constantes

---
+++ concatenacao.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+  <?php 
+
+  $nome = "Jorge";
+  $cor = 'Amarelo';
+  $roby = "Andar de skate";
+  $idade = 24;
+    echo 'Olá ' . $nome . ', vi que sua cor preferida é ' . $cor . ', estou vendo que voce possui ' . $idade . ' anos e gosta de ' . $roby;
+    echo '<br>';
+    echo "Olá $nome, vi que sua cor preferida é $cor, estou vendo que voce possui $idade de idade e gosta de $roby ";
+  ?>
+</body>
+</html>
